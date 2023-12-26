8d0963d5b0c1a9d531e8947da2ef9de55fa639bb
6-ON <aminhatim6@gmail.com>
2023-12-26 15:51:19 +0100

    Refactor auth module and controller

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -2,19 +2,17 @@ import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
-
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
 
-  @Post('login')
-  login(@Body() loginDto: LoginDto): void {
-    this.authService.login();
-  }
+	@Post('login')
+	login(@Body() loginDto: LoginDto): void {
+		this.authService.login();
+	}
 
-  @Get('refresh-token')
-  accessToken(): void {
-    this.authService.generateRefreshToken();
-  }
-
+	@Get('refresh-token')
+	accessToken(): void {
+		this.authService.generateRefreshToken();
+	}
 }
--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -3,26 +3,29 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+	constructor(private readonly jwtService: JwtService) {}
 
-  generateToken(payload: { username: string; sub: number }, expiresIn?: string): string {
-    return this.jwtService.sign(payload, { expiresIn });
-  }
+	generateToken(
+		payload: { username: string; sub: number },
+		expiresIn?: string,
+	): string {
+		return this.jwtService.sign(payload, { expiresIn });
+	}
 
-  login(): { access_token: string } {
-    const payload = { username: 'Hamid', sub: 1 };
-    return {
-      access_token: this.generateToken(payload),
-    };
-  }
+	login(): { access_token: string } {
+		const payload = { username: 'Hamid', sub: 1 };
+		return {
+			access_token: this.generateToken(payload),
+		};
+	}
 
-  generateAccessToken(): string {
-    const payload = { username: 'Hamid', sub: 1 };
-    return this.generateToken(payload);
-  }
+	generateAccessToken(): string {
+		const payload = { username: 'Hamid', sub: 1 };
+		return this.generateToken(payload);
+	}
 
-  generateRefreshToken(): string {
-    const payload = { username: 'Hamid', sub: 1 };
-    return this.generateToken(payload, '1d');
-  }
+	generateRefreshToken(): string {
+		const payload = { username: 'Hamid', sub: 1 };
+		return this.generateToken(payload, '1d');
+	}
 }
