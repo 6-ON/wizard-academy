e53ceb7c76153c4311cef8b2e45b09b097e6a712
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-26 09:55:40 +0100

    Remove unused imports and empty arrays in AppModule

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
