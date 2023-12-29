62b2575ba9a622f8af06e1c8831b31adfa435565
6-ON <aminhatim6@gmail.com>
2023-12-30 00:58:01 +0100

    Refactor TypeormConfigModule and TypeormConfigService

--- back-end/src/typeorm-config/typeorm-config.service.ts
+++ back-end/src/typeorm-config/typeorm-config.service.ts
@@ -6,16 +6,17 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 export class TypeormConfigService implements TypeOrmOptionsFactory {
 	constructor(private config: ConfigService) {}
 	createTypeOrmOptions(): TypeOrmModuleOptions {
-		console.log(this.config.get('DATABASE_HOST'));
-
 		return {
 			type: 'postgres',
 			host: this.config.get('DATABASE_HOST'),
-			port: this.config.get('DATABASE_PORT'),
+			port: +this.config.get('DATABASE_PORT'),
 			username: this.config.get('DATABASE_USERNAME'),
 			password: this.config.get('DATABASE_PASSWORD'),
 			database: this.config.get('DATABASE_NAME'),
 			autoLoadEntities: true,
+			logger: 'file',
+			logging: true,
+			// dropSchema: true, // TODO: remove this in production or when you want to persist data
 			synchronize: true,
 		};
 	}
