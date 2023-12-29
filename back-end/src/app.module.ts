c51e7ea328f98f9559cbb647bd1e295dcd532148
6-ON <aminhatim6@gmail.com>
2023-12-30 00:58:14 +0100

    Add User and Dean modules to app module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UniversityModule } from './university/university.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeormConfigModule } from './typeorm-config/typeorm-config.module';
 import { TypeormConfigService } from './typeorm-config/typeorm-config.service';
+import { UserModule } from './user/user.module';
+import { DeanModule } from './dean/dean.module';
 
 @Module({
 	imports: [
@@ -17,6 +19,8 @@ import { TypeormConfigService } from './typeorm-config/typeorm-config.service';
 			imports: [TypeormConfigModule],
 			useExisting: TypeormConfigService,
 		}),
+		UserModule,
+		DeanModule,
 	],
 	controllers: [],
 	providers: [],
