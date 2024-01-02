8f7309dc6bed59e3a886a1b249ce08361223afd3
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-01-02 14:38:19 +0100

    format

--- back-end/src/university/entities/university.entity.ts
+++ back-end/src/university/entities/university.entity.ts
@@ -1,5 +1,11 @@
 import { User } from '@/user/entities/user.entity';
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+	Column,
+	Entity,
+	JoinColumn,
+	OneToOne,
+	PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class University {
