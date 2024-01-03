01507fd14f25dbe40f570a0e29b973d592179908
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-01-03 12:10:05 +0100

    Add StudentModule to app.module.ts

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -7,20 +7,22 @@ import { TypeormConfigModule } from './typeorm-config/typeorm-config.module';
 import { TypeormConfigService } from './typeorm-config/typeorm-config.service';
 import { UserModule } from './user/user.module';
 import { DeanModule } from './dean/dean.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
 	imports: [
+		TypeOrmModule.forRootAsync({
+			imports: [TypeormConfigModule],
+			useExisting: TypeormConfigService,
+		}),
 		AuthModule,
 		UniversityModule,
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
-		TypeOrmModule.forRootAsync({
-			imports: [TypeormConfigModule],
-			useExisting: TypeormConfigService,
-		}),
 		UserModule,
 		DeanModule,
+		StudentModule,
 	],
 	controllers: [],
 	providers: [],
