38b4e5ee620eca2a64b64fe1532efa2b10824ef1
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-01-04 10:12:20 +0100

    Fix formatting in University entity and add unit tests in UniversityService

--- back-end/src/university/university.service.spec.ts
+++ back-end/src/university/university.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UniversityService } from './university.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { University } from './entities/university.entity';
+import { CreateUniversityDto } from './dto/create-university.dto';
+import { UserService } from '@/user/user.service';
+import { User } from '@/user/entities/user.entity';
 
 describe('UniversityService', () => {
 	let service: UniversityService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [UniversityService],
+			imports: [
+				TypeOrmModule.forRoot({
+					type: 'postgres',
+					host: 'localhost',
+					port: 5432,
+					username: 'postgres',
+					password: '123456',
+					logging: true,
+					logger: 'file',
+					database: 'TestWizard',
+					entities: ['src/**/*.entity{.ts,.js}'],
+					synchronize: true,
+					// dropSchema: true,
+				}),
+				TypeOrmModule.forFeature([University, User]),
+			],
+			providers: [UniversityService, UserService],
 		}).compile();
 
 		service = module.get<UniversityService>(UniversityService);
@@ -14,5 +35,33 @@ describe('UniversityService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+	it('should add new university', async () => {
+		const university: CreateUniversityDto = {
+			name: 'jfiore',
+			adress: 'gioeruiogre',
+			deanId: 4,
+		};
+		const createdUniversity = await service.create(university);
+		expect(createdUniversity).toBeInstanceOf(University);
+	});
+	it('should find all universities', async () => {
+		const universities = await service.findAll();
+		expect(universities).toBeInstanceOf(Array);
+	});
+	it('should find one university', async () => {
+		const university = await service.findOne(1);
+		expect(university).toBeInstanceOf(University);
+	});
+	it('Should update university', async () => {
+		const university = await service.update(1, {
+			name: 'test',
+			adress: 'trst',
+		});
+		expect(university).toBeInstanceOf(University);
+	});
+
+	it('should delete university', async () => {
+		await expect(async () => await service.remove(1)).rejects.toThrow();
 	});
 });
